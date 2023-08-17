c89ce6df56041aaba27541450d03a6d799725202
CaiooGomes <gomescaio898@gmail.com>
2023-08-17 17:21:58 -0300

    cards

---
+++ frontend/src/components/UI/AddNewButton.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+import './AddNewButton.css'
+function AddNewButton() {
+    const [mouse,SetMouse]= useState(false)
+    return(
+        <div 
+        onMouseEnter={()=>SetMouse(false)} 
+        onMouseLeave={()=>SetMouse(true)}
+        className={`${mouse ? "addNewButtom" : "addNewButton-mouse"}`}
+        >
+            {mouse ? "+" : "Adicionar uma anotação"}
+        </div>
+    )
+}
+
+export default AddNewButton
--- frontend/src/pages/Notes.jsx
+++ frontend/src/pages/Notes.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import "./Notes.css"
+import AddNewButton from '../components/UI/AddNewButton'
 
 function Notes() {
+  const [notes,setNotes] = useState([{title: "muito legal", description: "eu amei one piece", date: "8/09/2023", id: 1},{title: "muito legal", description: "eu amei one piece", date: "8/09/2023", id: 1},{title: "muito legal", description: "eu amei one piece", date: "8/09/2023", id: 1},{title: "muito legal", description: "eu amei one piece", date: "8/09/2023", id: 1},{title: "muito legal", description: "eu amei one piece", date: "8/09/2023", id: 1},{title: "muito legal", description: "eu amei one piece", date: "8/09/2023", id: 1},])
+
   return (
-    <div>Notes</div>
+    <div className='notes'>
+      <AddNewButton/>
+      {
+        notes.map(n=><div className='note'>
+                      <div>
+                        <h1>{n.title}</h1>
+                        <div className='desc'>{n.description}</div>
+                      </div>
+                        <div>
+                          <div>excluir</div>
+                          <div>edit</div>
+                        </div>
+                      </div>
+        )
+      }
+    </div>
   )
 }
 
